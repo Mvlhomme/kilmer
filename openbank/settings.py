31a96d3680754d9be0e41f02de40952406845f71
Malhomme <farrellndv770@gmail.com>
2023-01-03 00:04:18 +0100

    vercel commit

--- openbank/settings.py
+++ openbank/settings.py
@@ -72,7 +72,7 @@ AUTH_USER_MODEL = 'accounts.Account'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
